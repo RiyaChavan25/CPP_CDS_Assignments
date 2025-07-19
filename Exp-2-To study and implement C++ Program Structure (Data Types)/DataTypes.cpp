f19db90c04de2ac8e9b924a532ed7ea33168e0ef
RiyaChavan25 <riyachavan.work@gmail.com>
2025-07-19 17:18:13 +0530

    Create DataTypes.cpp

---
+++ Exp-2-To study and implement C++ Program Structure (Data Types)/DataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float pi = 3.14f;
+    double distance = 12345.678;
+    char grade = 'A';
+    bool pass = true;
+
+    cout << "Integer (age): " << age << "\n";
+    cout << "Float (pi): " << pi << "\n";
+    cout << "Double (distance): " << distance << "\n";
+    cout << "Char (grade): " << grade << "\n";
+    cout << "Boolean (pass): " << pass << "\n";
+
+    return 0;
+}
